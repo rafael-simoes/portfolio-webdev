01bf0118396fa7e8895fb0eebf2b7a5c21f8ec9c
rafael-simoes <blazerafa@gmail.com>
2021-09-12 14:54:50 +0100

    updated css

--- js/project.js
+++ js/project.js
@@ -8,31 +8,31 @@ let projects = [
     {
         name:"Music Player",
         tags:"#html #css #javascript",
-        image:"music.jpg",
+        image:"music_player.jpg",
         link:"https://musicplayerrafael.netlify.app/"
     },
     {
         name:"Translation Freelancer",
         tags:"#html #css #javascript #ui/ux",
-        image:"translation.jpg",
+        image:"sandra_rosa.jpg",
         link:"https://sandrarosatranslator.netlify.app/",
     },
     {
         name:"Gradient Picker",
         tags:"#javascript #html #css",
-        image:"gradient.jpg",
+        image:"gradient_picker.jpg",
         link:"https://gradientpickerrafael.netlify.app/",
     },
     {
         name:"Color Picker",
         tags:"#html #javascript #css",
-        image:"picker.jpg",
+        image:"color_converter.jpg",
         link:"https://colorchangerrafael.netlify.app/",
     },
     {
         name:"To Do App",
         tags:"#outsystems #mobile",
-        image:"to do.jpg",
+        image:"todo.gif",
         link:"https://rafael-simoes92.outsystemscloud.com/PreviewInDevices/?IsMobilePreview=True&DeviceName=Smartphone&URL=/ToDoRS/Primary?_ts=637318244914484198"
     }
 ]
